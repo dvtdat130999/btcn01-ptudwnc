9571791eb2eec36a0bbf06a54e2a00285fc7b31a
dvtdat130999 <50757817+dvtdat130999@users.noreply.github.com>
2020-10-06 19:49:46 +0700

    Updates

---
+++ precache-manifest.d1c92fab3bec8baf015c0322b9013b78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48a81cebe39b9c7d92abbab3cb34cab8",
+    "url": "/btcn01-ptudwnc/index.html"
+  },
+  {
+    "revision": "b533c9c779433e7a640a",
+    "url": "/btcn01-ptudwnc/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3bce6c4fb451e9df3fdb",
+    "url": "/btcn01-ptudwnc/static/js/2.882341a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/btcn01-ptudwnc/static/js/2.882341a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b533c9c779433e7a640a",
+    "url": "/btcn01-ptudwnc/static/js/main.3b841e29.chunk.js"
+  },
+  {
+    "revision": "a10108e9cdf0e69dfc87",
+    "url": "/btcn01-ptudwnc/static/js/runtime-main.6a7363ad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/btcn01-ptudwnc/static/media/logo.5d5d9eef.svg"
+  }
+]);
